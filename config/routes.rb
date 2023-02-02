ddd2bbe4c1cd8331703e6d312ab69c5b9e75189c
shivampatidar2000 <shivampatidar2112@gmail.com>
2023-02-02 15:42:08 +0530

    six

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "user#index"
+
+  resources :posts
+
+  # post "/posts/new" => "post#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
